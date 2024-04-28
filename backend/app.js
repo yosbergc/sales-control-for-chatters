17274c7e2c848115765fe10be78cc3a15927506c
Yosber Guerrero <yosbergc@gmail.com>
2024-04-27 21:24:46 -0400

    Added some things

--- backend/app.js
+++ backend/app.js
@@ -8,8 +8,21 @@ app.use(express.json());
 // Agregamos el path de la variable ENV.
 dotenv.config({path: "./env/.env"})
 // Seteamos el directorio público
-app.use('/resources', express.static('public'))
-app.use('/resources', express.static(__dirname + '/public'))
+app.use('/resources', express.static('public'));
+app.use('/resources', express.static(__dirname + '/public'));
+// Asignamos el motor de plantillas
+app.set('view engine', 'ejs');
+
+// Invocamos el módulo de hashing de passwword.
+const bcrypt = require('bcryptjs');
+// Configuramos las variables de sesión
+
+const session = require('express-session');
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}))
 
 console.log(__dirname + '/public')
 app.listen(3000, (request, response) => {
