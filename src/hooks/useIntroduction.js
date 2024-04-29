fb47d2a49f7beb4044dbe77d88ec020f69640958
Yosber Guerrero <yosbergc@gmail.com>
2024-04-28 20:05:56 -0400

    Added

--- src/hooks/useIntroduction.js
+++ src/hooks/useIntroduction.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 /* eslint-disable react-hooks/exhaustive-deps */
 import {useEffect, useState} from 'react'
 import { useLocalStorage } from './useLocalStorage';
@@ -5,22 +6,36 @@ function useIntroduction ({setMetaSemanal}) {
     const {get, parse, set} = useLocalStorage();
     const [userNew, setUserNew] = useState();
     const DB = get('userLogged')
+=======
+import {useEffect, useState} from 'react'
+function useIntroduction () {
+    const DB = localStorage.getItem('userLogged')
+    const [userNew, setUserNew] = useState();
+>>>>>>> Stashed changes
     useEffect(() => {
         if (!DB) {
             setUserNew(true);
         } else {
+<<<<<<< Updated upstream
             const userInformation = parse('userInformation');
+=======
+            let userInformation = JSON.parse(localStorage.getItem('userInformation'));
+>>>>>>> Stashed changes
             setUserNew(userInformation)
         }
     }, [])
     
     function addNewUser(nombre, genero, meta) {
+<<<<<<< Updated upstream
         console.log(nombre, genero, meta)
+=======
+>>>>>>> Stashed changes
         let newUser = {
             nombre,
             genero,
             meta
         }
+<<<<<<< Updated upstream
         set('userInformation', newUser)
         setUserNew(newUser)
         set('userLogged', true)
@@ -32,6 +47,12 @@ function useIntroduction ({setMetaSemanal}) {
           setMetaSemanal(data.meta)
       }, [userNew])
     
+=======
+        localStorage.setItem('userInformation', JSON.stringify(newUser))
+        setUserNew(newUser)
+        localStorage.setItem('userLogged', JSON.stringify(true));
+    }
+>>>>>>> Stashed changes
     return {userNew, addNewUser}
 }
 export { useIntroduction }
