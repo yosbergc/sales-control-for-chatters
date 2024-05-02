ed7fa0d9bc783772ccbaf7f590a339bf3b14fdbf
Yosber Guerrero <yosbergc@gmail.com>
2024-05-02 07:38:24 -0400

    Added menu component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
 import ProfilePhoto from '../../images/profile.jpeg'
+import { Menu } from '../Menu/Menu'
 import './header.css'
 function Header () {
     return (
         <header>
-          <section>
-
-          </section>
+          <Menu>
+            <a href='#xs'>Dashboard</a>
+            <a href='#xs'>Horario</a>
+            <a href='#xs'>Notificaciones</a>
+            <a href='#xs'>Notas</a>
+          </Menu>
           <section className='user-header'>
             <img src={ProfilePhoto} alt='User Name'/>
             <p>Macaco</p>
---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,7 @@
+import './Menu.css'
+function Menu ({ children }) {
+    return (<section className="menu">
+        {children}
+    </section>)
+}
+export { Menu }
