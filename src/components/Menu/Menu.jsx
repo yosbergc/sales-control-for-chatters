3888c5213c0cf69fb32a039aa19bf83ab042b645
Yosber Guerrero <yosbergc@gmail.com>
2024-05-03 17:38:46 -0400

    Added responsive menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,7 +1,17 @@
 import './Menu.css'
+import { IoIosMenu } from "react-icons/io"
+import React from 'react'
 function Menu ({ children }) {
-    return (<section className="menu">
-        {children}
+    const [isOpen, setIsOpen] = React.useState(false)
+    const handleClick = () => {
+        setIsOpen(!isOpen)
+    }
+    return (
+    <section className='menu-container'>
+        <IoIosMenu size={30} color='white' className='menuIcon' onClick={handleClick}/>
+        <section className={`menu ${isOpen && 'mobile-active'}`} >
+            {children}
+        </section>
     </section>)
 }
 export { Menu }
