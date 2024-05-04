a424f5378ce135d3fe54ebd828f2efcd5fbf3efc
Yosber Guerrero <yosbergc@gmail.com>
2024-05-04 11:04:49 -0400

    Added pages components

--- src/index.js
+++ src/index.js
@@ -3,14 +3,23 @@ import './index.css';
 import App from './routes/DashboardPage';
 import { LoginPage } from './routes/LoginPage';
 import { SchedulePage } from './routes/SchedulePage'
+import { MessagePage } from './routes/MessagesPage'
+import { EarningPages } from './routes/EarningsPage'
+import { AccountPage } from './routes/AccountPage'
+import { ProfilePage } from './routes/ProfilePage'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Router>
         <Routes>
             <Route path='/' element={<LoginPage/>}/>
-            <Route path='/dashboard' element={<App/>}/>
-            <Route path='/dashboard/horario' element={<SchedulePage/>}/>
+            <Route path='/panel' element={<App/>}/>
+            <Route path='/panel/horario' element={<SchedulePage/>}/>
+            <Route path='/panel/mensajes' element={<MessagePage/>}/>
+            <Route path='/panel/ganancias' element={<EarningPages/>}/>
+            <Route path='/cuenta' element={<AccountPage/>}/>
+            <Route path='/perfil' element={<ProfilePage/>}/>
         </Routes>
     </Router>
 );
